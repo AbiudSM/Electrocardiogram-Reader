3d9a61a035f96f8d09494a9d6b41cd14b2776e18
Robertudo <sayamosfero@gmail.com>
2022-11-29 16:24:48 -0600

    add interface to make predictions on screenshot

--- main.py
+++ main.py
@@ -1,110 +1,84 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image
+from PIL import ImageTk
 import cv2
+import imutils
 import numpy as np
-import ECG_CNN as cnn
+import pyautogui
+import threading
 
-# Electrocardiogram full image analyze_image function
-# ? returns the image with the ecgs at risk indicated by a red rectangle
-def analyze_image(model_path, img):
+screen_recorded = False
 
-    risk_image = False
-        
-    # Prediction colors, 0 -> sano, 1 -> onda S, 2 -> onda T, 3 -> onda Q
-    risk_colors = [None, (255,0,0), (255,255,0), (0,0,255), ]
-    risk_text = ['Sano', 'Onda-S', 'Onda-T', 'Onda-Q']
-    
-    # img = cv2.resize(img, (width,height), interpolation = cv2.INTER_AREA)
+def record_screen():
+	global screen_recorded
+	global image
 
-    coords, img = yolo_prediction(img)
+	# Label IMAGEN DE SALIDA
+	text_output_image = Label(root, text="OUTPUT IMAGE:", font="bold")
+	text_output_image.grid(column=1, row=0, padx=5, pady=5)
 
-    for pts in coords:
-        pt1, pt2 = pts
-        cutted_image = img[pt1[1]:pt2[1], pt1[0]:pt2[0]]
-        
-        prediction = int(cnn.prediction(model_path,cutted_image))
+	screen_recorded = True
 
-        if prediction != 0:
-            risk_image = cv2.rectangle(img, pt1, pt2, risk_colors[prediction], 3)
-            cv2.putText(img, risk_text[prediction], (pt1[0],pt1[1]+30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, risk_colors[prediction], 2)
+	while screen_recorded == True:
+		image = np.array(pyautogui.screenshot()) 
+		image = cv2.resize(image, (1000,600))
 
-    return risk_image
+		umbral_1 = first_threshold.get()
+		umbral_2 = second_threshold.get()
 
+		grises = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		bordes = cv2.Canny(grises, umbral_1, umbral_2)
 
-def yolo_prediction(img):
-    # Load Yolo
-    net = cv2.dnn.readNet("yolov3/yolov3_training_3000.weights", "yolov3/yolov3_testing.cfg")
-
-    # Name custom object
-    classes = ["ecg"]
-
-    layer_names = net.getLayerNames()
-    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-    coords = list()
-
-    # img = cv2.resize(img, None, fx=0.4, fy=0.4)
-    # img = cv2.resize(img, dsize=(1280,720))
-
-    height, width, channels = img.shape
-
-    # Detecting objects
-    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-
-    net.setInput(blob)
-    outs = net.forward(output_layers)
-
-    # Showing informations on the screen
-    class_ids = []
-    confidences = []
-    boxes = []
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.3:
-                # Object detected
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-
-                # Rectangle coordinates
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-
-    # ? show predictions image
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    font = cv2.FONT_HERSHEY_PLAIN
-    # detection_image = img.copy()
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-
-            coords.append([[x,abs(y)],[x + w, y + h]])
-            
-            # label = str(classes[class_ids[i]])
-            # color = colors[class_ids[i]]
-            # cv2.rectangle(detection_image, (x, y), (x + w, y + h), color, 2)
-            # cv2.putText(detection_image, label, (x, y + 30), font, 3, color, 2)
+		# Para visualizar la imagen en label_output_image en la GUI
+		im = Image.fromarray(bordes)
+		img = ImageTk.PhotoImage(image=im)
+		label_output_image.configure(image=img)
+		label_output_image.image = img
 
 
-    # cv2.imshow("detection_image", detection_image)
-    # key = cv2.waitKey(0)
-
-    # cv2.destroyAllWindows()
-
-    return coords, img
+	label_output_image.configure(image='')
+	label_output_image.image = ''
+	text_output_image.grid_forget()
 
 
-risk_image = analyze_image('models/ondas-buff.hdf5', cv2.imread('src/full/anormal-T-cut.jpg'))
-if type(risk_image) == bool:
-    print('\n\nElectrocardiograma completamenete sano')
-else:
-    cv2.imshow('risk_image',risk_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def record_screen_thread():
+	threading.Thread(target=record_screen, daemon = True).start()
+
+
+def stop_button_screen_recording():
+	global screen_recorded
+	screen_recorded = False
+
+
+# Root window
+root = Tk()
+root.geometry('1280x720')
+root.title('ECG reader')
+
+
+label_output_image = Label(root)
+label_output_image.grid(column=1, row=1, rowspan=6)
+
+
+# Thresholds
+label_thresholds = Label(root, text="UMBRALES", width=25)
+label_thresholds.grid(column=0, row=3, padx=5, pady=5)
+
+
+first_threshold = Scale(root, from_=0, to=1000, orient=HORIZONTAL)
+first_threshold.grid(column=0, row=4)
+
+second_threshold = Scale(root, from_=0, to=1000, orient=HORIZONTAL)
+second_threshold.grid(column=0, row=5)
+
+
+# Buttons
+record_screen_button = Button(root, text="Record screen", width=25, command=record_screen_thread)
+record_screen_button.grid(column=0, row=0, padx=5, pady=5)
+
+stop_button = Button(root, text="stop_button screeen recording", width=25, command=stop_button_screen_recording)
+stop_button.grid(column=0, row=1, padx=5, pady=5)
+
+
+root.mainloop()
---
+++ methods.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+import ECG_CNN as cnn
+from config import YOLO_CONFIG, YOLO_MODEL
+
+# Electrocardiogram full image analyze_image function
+# ? returns the image with the ecgs at risk indicated by a red rectangle
+def analyze_image(model_path, img):
+
+	# Prediction colors, 0 -> sano, 1 -> onda S, 2 -> onda T, 3 -> onda Q
+	risk_colors = [None, (255,0,0), (255,255,0), (0,0,255), ]
+	risk_text = ['Sano', 'Onda-S', 'Onda-T', 'Onda-Q']
+	
+	coords, img = yolo_prediction(img)
+
+	for pts in coords:
+		pt1, pt2 = pts
+		cutted_image = img[pt1[1]:pt2[1], pt1[0]:pt2[0]]
+		
+		prediction = int(cnn.prediction(model_path,cutted_image))
+
+		if prediction != 0:
+			img = cv2.rectangle(img, pt1, pt2, risk_colors[prediction], 3)
+			cv2.putText(img, risk_text[prediction], (pt1[0],pt1[1]+30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, risk_colors[prediction], 2)
+
+	return img
+
+
+def yolo_prediction(img):
+	# Load Yolo
+	net = cv2.dnn.readNet(YOLO_MODEL, YOLO_CONFIG)
+
+
+	layer_names = net.getLayerNames()
+	output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+
+	coords = list()
+
+	height, width, channels = img.shape
+
+	# Detecting objects
+	blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+	net.setInput(blob)
+	outs = net.forward(output_layers)
+
+	# Showing informations on the screen
+	class_ids = []
+	confidences = []
+	boxes = []
+	for out in outs:
+		for detection in out:
+			scores = detection[5:]
+			class_id = np.argmax(scores)
+			confidence = scores[class_id]
+			if confidence > 0.3:
+				# Object detected
+				center_x = int(detection[0] * width)
+				center_y = int(detection[1] * height)
+				w = int(detection[2] * width)
+				h = int(detection[3] * height)
+
+				# Rectangle coordinates
+				x = int(center_x - w / 2)
+				y = int(center_y - h / 2)
+
+				boxes.append([x, y, w, h])
+				confidences.append(float(confidence))
+				class_ids.append(class_id)
+
+	indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+	for i in range(len(boxes)):
+		if i in indexes:
+			x, y, w, h = boxes[i]
+			
+			pt1 = [x,y]
+			pt2 = [x + w, y + h]
+
+			# ? if all corrds are positive
+			if x > 0 and y > 0 and pt2[0] > 0 and pt2[1] > 0:
+				coords.append([pt1,pt2])
+
+	return coords, img
